0be318c2ea36a11a2318884bb5a63095ffd4cb19
Carolina <carolina.arcos@wolox.co>
2020-01-02 01:09:33 -0500

    Add carousel view

--- Cookbook-ios/AppDelegate.swift
+++ Cookbook-ios/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let rootViewController = CarouselViewController(nibName: ViewNames.carouselView, bundle: nil)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
         return true
     }
 
