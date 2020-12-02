a42599e54d96bdf4ab7f9adc0ebcc683c6e2ac85
Daniel <daniel.charry@wolox.co>
2020-12-02 17:28:49 -0500

    Rename vars and organize code

--- Cookbook-ios/ShadowView/ShadowViewController.swift
+++ Cookbook-ios/ShadowView/ShadowViewController.swift
@@ -10,73 +10,100 @@ import UIKit
 
 class ShadowViewController: GeneralViewController {
     
-    @IBOutlet weak var prueba1: UIView!
-    
-    @IBOutlet weak var prueba3: UIView!
-    
+    // MARK: - IBOutlet
+    @IBOutlet weak var testRectangelView: UIView!
+    @IBOutlet weak var testCircleView: UIView!
     @IBOutlet weak var slideRadius: UISlider!
     @IBOutlet weak var slideSize: UISlider!
     @IBOutlet weak var slideOpacity: UISlider!
     @IBOutlet weak var slideCornerRadius: UISlider!
-    
-    @IBOutlet weak var colorContainer: UIStackView!
-    
-    private var size: CGSize?
-    private var radius: CGFloat?
-    private var opacity: Float?
-    private var cornerRadius: CGFloat?
-    private var color: CGColor?
-    
-    private var colorPrevio = [UIColor.red, UIColor.black, UIColor.blue, UIColor.yellow, UIColor.green, UIColor.orange]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setUpUI()
-        createColors()
-    }
+    @IBOutlet weak var colorFirst: UIButton!
+    @IBOutlet weak var colorSecond: UIButton!
+    @IBOutlet weak var colorThird: UIButton!
     
     @IBAction func sliderRadiusChanged(_ sender: Any) {
         radius = CGFloat(slideRadius.value * 2)
-        prueba1.applyShadow(color: color, size: size, radius: radius, opacity: opacity, cornerRadius: cornerRadius)
+        testRectangelView.applyShadow(color: selectColor?.cgColor,
+                                      size: size,
+                                      radius: radius,
+                                      opacity: opacity,
+                                      cornerRadius: cornerRadius)
         
     }
+    
     @IBAction func sliderSizeChanged(_ sender: Any) {
-        var w = Int(slideSize.value * -100)
-        var h = Int(slideSize.value * 100)
-        size = CGSize(width: w, height: h)
-        prueba1.applyShadow(color: color, size: size, radius: radius, opacity: opacity, cornerRadius: cornerRadius)
+        let width = Int(slideSize.value * -10)
+        let height = Int(slideSize.value * 10)
+        size = CGSize(width: width, height: height)
+        testRectangelView.applyShadow(color: selectColor?.cgColor,
+                                      size: size,
+                                      radius: radius,
+                                      opacity: opacity,
+                                      cornerRadius: cornerRadius)
     }
     
     @IBAction func sliderOpacityChanged(_ sender: Any) {
         opacity = Float(slideOpacity.value)
-        prueba1.applyShadow(color: color, size: size, radius: radius, opacity: opacity, cornerRadius: cornerRadius)
+        testRectangelView.applyShadow(color: selectColor?.cgColor,
+                                      size: size,
+                                      radius: radius,
+                                      opacity: opacity,
+                                      cornerRadius: cornerRadius)
     }
     
     @IBAction func sliderCornerRadiusChanged(_ sender: Any) {
         cornerRadius = CGFloat(slideCornerRadius.value * 60)
-        prueba1.applyShadow(color: color, size: size, radius: radius, opacity: opacity, cornerRadius: cornerRadius)
+        testRectangelView.applyShadow(color: selectColor?.cgColor,
+                                      size: size, radius: radius,
+                                      opacity: opacity,
+                                      cornerRadius: cornerRadius)
     }
     
+    @IBAction func pressFirstButtonChangedColor(_ sender: Any) {
+        selectColor = UIColor.black
+        testRectangelView.applyShadow(color: selectColor?.cgColor,
+                                      size: size,
+                                      radius: radius,
+                                      opacity: opacity,
+                                      cornerRadius: cornerRadius)
+    }
     
+    @IBAction func pressSecondButtonChangeColor(_ sender: Any) {
+        selectColor = UIColor.red
+        testRectangelView.applyShadow(color: selectColor?.cgColor,
+                                      size: size,
+                                      radius: radius,
+                                      opacity: opacity,
+                                      cornerRadius: cornerRadius)
+    }
     
+    @IBAction func pressThirdButtonChangeColor(_ sender: Any) {
+        selectColor = UIColor.yellow
+        testRectangelView.applyShadow(color: selectColor?.cgColor,
+                                      size: size,
+                                      radius: radius,
+                                      opacity: opacity,
+                                      cornerRadius: cornerRadius)
+    }
+    
+    // MARK: - Private vars
+    private var size: CGSize?
+    private var radius: CGFloat?
+    private var opacity: Float?
+    private var cornerRadius: CGFloat?
+    private var selectColor: UIColor?
+    private var color: CGColor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpUI()
+    }
 }
 
 extension ShadowViewController {
     func setUpUI() {
-        
-        prueba1.applyShadow()
-
-        prueba3.applyShadow(color: UIColor.red.cgColor, cornerRadius: prueba3.frame.width / 2)
-    }
-    
-    func createColors() {
-        colorContainer.arrangedSubviews.enumerated().forEach {
-            $0.element.backgroundColor = colorPrevio[$0.offset]
-        }
-    }
-    
-    func gestureTap() {
-//        let tap = UIGestureRecognizer(target: self, action: #selector(han))
-//        colorContainer.arrangedSubviews[0].addGestureRecognizer(tap)
+        testRectangelView.applyShadow()
+        testCircleView.applyShadow(color: UIColor.red.cgColor,
+                                   cornerRadius: testCircleView.frame.width / 2)
     }
 }
